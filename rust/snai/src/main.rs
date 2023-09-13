5fa971851d50d19bcdd2169ca6ae6b10e9c401db
ShorterThanDijkstra <db1345569965@gmail.com>
2023-09-13 17:05:20 +0800

    snail

--- rust/snai/src/main.rs
+++ rust/snai/src/main.rs
@@ -3,12 +3,15 @@ fn snail(matrix: &[Vec<i32>]) -> Vec<i32> {
     if n == 0 {
         return Vec::new();
     }
+    if n == 1 && matrix[0].len() == 0 {
+        return Vec::new();
+    }
     let mut start_x = 0;
     let mut start_y = 0;
     let mut steps = n - 1;
     let mut v = Vec::new();
     while steps > 0 {
-        println!("{} {} {}", start_x, start_y, steps);
+        // println!("{} {} {}", start_x, start_y, steps);
         right(steps, start_x, start_y, &mut v, matrix);
         down(steps, start_x, start_y + steps, &mut v, matrix);
         left(steps, n - 1 - start_x, n - 1 - start_y, &mut v, matrix);
@@ -16,6 +19,9 @@ fn snail(matrix: &[Vec<i32>]) -> Vec<i32> {
         start_x += 1;
         start_y += 1;
         steps = if steps > 1 { steps - 2 } else { 0 }
+    }
+    if n % 2 != 0 {
+        v.push(matrix[start_x][start_y])
     }
     v
 }
@@ -40,12 +46,12 @@ fn up(steps: usize, x: usize, y: usize, v: &mut Vec<i32>, matrix: &[Vec<i32>]) {
     }
 }
 fn main() {
-    // let matrix = &[
-    //     vec![1, 2, 3, 1],
-    //     vec![4, 5, 6, 4],
-    //     vec![7, 8, 9, 7],
-    //     vec![7, 8, 9, 7],
-    // ];
-    let matrix = &[vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+    let matrix = &[
+        vec![1, 2, 3, 1],
+        vec![4, 5, 6, 4],
+        vec![7, 8, 9, 7],
+        vec![7, 8, 9, 7],
+    ];
+    // let matrix = &[vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
     println!("{:?}", snail(matrix))
 }
