6d982a9afd6036bba1714392b799d2570e56eac2
ShorterThanDijkstra <db1345569965@gmail.com>
2023-10-12 17:42:50 +0800

    wip: string_incrementer.c

--- c/string_incrementer.c
+++ c/string_incrementer.c
@@ -1,29 +1,66 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 char *incrementString(const char *str)
 {
-    const char *num_head = NULL;
-    while (*str != '\0')
+    int split = -1;
+    int i = 0;
+    while (str[i] != '\0')
     {
-        printf("%s\n", num_head);
-        if (isdigit(*str) && num_head == NULL)
+        if (isdigit(str[i]))
         {
-            num_head = str;
+            if (split == -1)
+            {
+                split = i;
+            }
         }
         else
         {
-            num_head = NULL;
+            split = -1;
         }
-        str += 1;
+        i += 1;
     }
-    return (char *)num_head;
+    // printf("%d\n", split);
+    // printf("%d\n", i);
+
+    if (split == -1)
+    {
+        char *res = (char *)malloc((i + 2) * sizeof(char));
+        strncpy(res, str, i);
+        res[i] = '1';
+        res[i + 1] = '\0';
+        return res;
+    }
+    else
+    {
+        int trailing = atoi(str + split - 1) + 1;
+        int leading_0 = 0;
+        while (str[leading_0 + split] == '0')
+        {
+            leading_0 += 1;
+        }
+        if (leading_0 == 0)
+        {
+            printf("%d\n", split);
+            int len = split + 
+            char* sub = (char *) malloc(split  * sizeof(char));
+            strncpy(sub, str, split);
+            puts(sub);
+            char *res = strcat(sub, itoa(trailing));
+            puts(res);
+        }
+        
+    }
+
+    return NULL;
 }
 int main()
 {
-    char *s1 = "hell92h00901";
-    printf("%s\n", incrementString(s1));
+    char *s1 = "hell3";
+    incrementString(s1);
+    // puts(incrementString(s1));
     // char c = 'a';
-    // printf("%d\n", isdigit(c));
+    // printf("%d\n", atoi(s1+4));
 }
