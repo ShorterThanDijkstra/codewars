e390c9127e23b161bca72466ecfd31d0b2107f57
maerd-zinbieL <db1345569965@gmail.com>
2023-12-14 20:34:22 +0800

    PersistentBugger

---
+++ scala/PersistentBugger.scala
@@ -0,0 +1,15 @@
+object Multiplication:
+
+    def mulDigits(n: Int): Int = 
+        if n < 10 then n
+        else (n % 10) * mulDigits(n / 10)
+
+    def persistence(n: Int): Int = 
+        if n < 10 then 0
+        else {val res = mulDigits(n); println(res); 1 + persistence(res)}
+
+@main
+def main()=
+    val res = Multiplication.persistence(999)
+    println(res)
+        
