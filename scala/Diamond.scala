7b3e95af8b7cfbc6652ad7872c53f0de8b9145ab
maerd-zinbieL <db1345569965@gmail.com>
2023-12-16 00:23:11 +0800

    Diamond

--- scala/Diamond.scala
+++ scala/Diamond.scala
@@ -1,7 +1,26 @@
 def build(n: Int): String =
-  def buildLine(i: Int): String =
-    if i< n / 2 then {val star = (n - 1) / 2; val space
+  def buildFirstLines(space: Int, star: Int): String =
+    if star == n then "*" * n ++ "\n"
+    else
+      " " * space ++ "*" * star ++ "\n" ++ buildFirstLines(
+        space - 1,
+        star + 2
+      )
+  def buildSecondLines(space: Int, star: Int): String =
+    if star == 1 then " " * space ++ "*" ++ "\n"
+    else
+      " " * space ++ "*" * star ++ "\n" ++ buildSecondLines(
+        space + 1,
+        star - 2
+      )
+  if n == 1 then "*\n"
+  else buildFirstLines((n - 1) / 2, 1) ++ buildSecondLines(1, n - 2)
+
 def diamond(n: Int): Option[String] =
-  if n < 0 || n % 2 == 0
+  if n <= 0 || n % 2 == 0
   then None
   else Some(build(n))
+
+@main
+def main() = 
+  println(diamond(1))
