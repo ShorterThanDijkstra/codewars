b8b6d3510f7212ea39c92f91cc646740f389be5e
ShorterThanDijkstra <db1345569965@gmail.com>
2023-12-25 20:59:14 +0800

    wip

--- haskell/MergedStringChecker.hs
+++ haskell/MergedStringChecker.hs
@@ -1,7 +1,8 @@
 -- https://www.codewars.com/kata/54c9fcad28ec4c6e680011aa/train/haskell
 
 module MergedStringChecker where
+
 isMerge :: String -> String -> String -> Bool
 isMerge [] [] [] = True
-isMerge [] _  _ = False
-isMerge (c:cs) (c1:cs1) (c2:cs2) | c==c1 = isMerge cs 
+isMerge [] _ _ = False
+isMerge (c : cs) (c1 : cs1) (c2 : cs2) | c == c1 && c == c2 = isMerge cs cs1 (c2 : cs2) || isMerge cs (c1 : cs1) cs2
