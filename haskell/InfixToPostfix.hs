12df2e75f0d1a74277c5a2902b7884bc3dae87fe
maerd-zinbieL <db1345569965@gmail.com>
2024-04-15 20:36:58 +0800

    wip

--- haskell/InfixToPostfix.hs
+++ haskell/InfixToPostfix.hs
@@ -2,22 +2,84 @@ module InfixToPostfix where
 
 import Control.Monad.State (State)
 import Data.Char (ord)
+import Debug.Trace (trace)
 import Text.Read (Lexeme (String))
 
---  Expr -> AddSub
---  AddSub -> AddSub + MulDiv
---  AddSub -> MulDiv
---  MulDiv -> MulDiv * Term
---  MulDiv -> Term
+-- 左结合+优先级：
+--  Expr -> Add
+--  Add -> Add + Mul
+--  Add -> Mul
+--  Mul -> Mul * Term
+--  Mul -> Term
 --  Term -> Int
 
-newtype Term = Term Int 
+-- 消除左递归：
+--  Expr -> Add
+--  Add -> Mul Add'
+--  Add' -> + Mul Add'
+--  Add' -> epsilon
+--  Mul -> Term Mul'
+--  Mul' -> * Term Mul'
+--  Mul' -> epsilon
+--  Term -> Int
 
-data MulDiv = MulDivTerm Term | Mul MulDiv Term | Div MulDiv Term
+newtype Expr = Expr AddSub deriving (Show)
 
-data AddSub = AddSubMulDiv MulDiv | Add AddSub MulDiv | Sub AddSub MulDiv
+data AddSub = AddSub MulDiv AddSub' deriving (Show)
 
-newtype Expr = Expr AddSub
+data AddSub' = Add MulDiv AddSub' | Sub MulDiv AddSub' | AddSubEmpty' deriving (Show)
+
+data MulDiv = MulDiv Term MulDiv' deriving (Show)
+
+data MulDiv' = Mul Term MulDiv' | Div Term MulDiv' | MulDivEmpty' deriving (Show)
+
+newtype Term = Term Int deriving (Show)
+
+parseExpr :: String -> Expr
+parseExpr s =
+  let ("", e) = parseAddSub s
+   in Expr e
+
+parseAddSub :: String -> (String, AddSub)
+parseAddSub s =
+  let (s1, muldiv) = parseMulDiv s
+      (s2, addsub') = parseAddSub' s1
+   in (s2, AddSub muldiv addsub')
+
+parseAddSub' :: String -> (String, AddSub')
+parseAddSub' [] = ("", AddSubEmpty')
+parseAddSub' (c : s) 
+  | c == '+' || c == '-' = 
+  let (s1, muldiv) = parseMulDiv s
+      (s2, addsub') = parseAddSub' s1
+   in (s2, op c muldiv addsub')
+
+  | otherwise = (c:s,  AddSubEmpty')
+  where
+    op '+' = Add
+    op '-' = Sub
+parseMulDiv :: String -> (String, MulDiv)
+parseMulDiv s =
+  let (s1, term) = parseTerm s
+      (s2, muldiv') = parseMulDiv' s1
+   in (s2, MulDiv term muldiv')
+
+parseMulDiv' :: String -> (String, MulDiv')
+parseMulDiv' [] = ("", MulDivEmpty')
+parseMulDiv' (c : s)
+  | c == '*' || c == '/' =
+      let (s1, term) = parseTerm s
+          (s2, muldiv') = parseMulDiv' s1
+       in (s2, op c term muldiv')
+  | otherwise = (c : s, MulDivEmpty')
+
+  where
+    op '*' = Mul
+    op '/' = Div
+
+parseTerm :: String -> (String, Term)
+parseTerm (c : s) | c <= '9' && c >= '0' = (s, Term (ord c - ord '0'))
+
 -- parseParentheses :: String -> (String, Expr)
 -- parseParentheses s =
 --   let (s1, e) = parseExpr s
@@ -40,12 +102,12 @@ newtype Expr = Expr AddSub
 -- parseExpr [] = error "bad input"
 -- parseExpr (c : s)
 --   | c <= '9' && c >= '0' && null s = ("", Value $ ord c - ord '0')
-  
+
 --   | c == '(' =
 --       withRightOrDefault $ parseParentheses s
 --   | otherwise = let (s1, l) = parseExpr (c:s)
 --                     (s2, op) = parseOp s1
---                     (s3, r) = parseExpr s2 
+--                     (s3, r) = parseExpr s2
 --                 in (s3, op l r)
 --   where
 --     withRightOrDefault (s, l) =
