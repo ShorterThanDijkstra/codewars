6c3dee013902bcd41d4102ac7b949b12bd1cbc54
maerd-zinbieL <db1345569965@gmail.com>
2024-04-20 15:04:31 +0800

    wip

--- haskell/InfixToPostfix.hs
+++ haskell/InfixToPostfix.hs
@@ -1,7 +1,7 @@
 module InfixToPostfix where
 
-import Control.Monad.State (State)
-import Data.Char (ord, chr)
+import Control.Monad.State (State, evalState, get, put, runState)
+import Data.Char (chr, ord)
 
 -- 左结合+优先级：
 --  Expr -> Add
@@ -77,13 +77,87 @@ parseTerm (c : s)
        in (s1, e)
   | otherwise = error $ c : s
 
+parseExprS :: State String Expr
+parseExprS = parseAddSubS
+
+parseAddSubS :: State String Expr
+parseAddSubS = do
+  muldiv <- parseMulDivS
+  parseAddSubS' muldiv
+
+parseAddSubS' :: Expr -> State String Expr
+parseAddSubS' expr = do
+  s <- get
+  case s of
+    ('+' : s1) -> do
+      put s1
+      muldiv <- parseMulDivS
+      parseAddSubS' (Add expr muldiv)
+    ('-' : s1) -> do
+      put s1
+      muldiv <- parseMulDivS
+      parseAddSubS' (Sub expr muldiv)
+    _ -> return expr
+
+parseMulDivS :: State String Expr
+parseMulDivS = do
+  expo <- parseExpoS
+  parseMulDivS' expo
+
+parseMulDivS' :: Expr -> State String Expr
+parseMulDivS' expr = do
+  s <- get
+  case s of
+    ('*' : s1) -> do
+      put s1
+      expo <- parseExpoS
+      parseMulDivS' (Mul expr expo)
+    ('/' : s1) -> do
+      put s1
+      expo <- parseExpoS
+      parseMulDivS' (Div expr expo)
+    _ -> return expr
+
+parseExpoS :: State String Expr
+parseExpoS = do
+  base <- parseTermS
+  s <- get
+  case s of
+    ('^' : s1) -> do 
+                   put s1 
+                   Expo base <$> parseExpoS
+    _ -> return base
+
+parseTermS :: State String Expr
+parseTermS = do
+  s <- get
+  case s of
+    (c : s1) -> do
+      put s1
+      if c <= '9' && c >= '0'
+        then return $ Term $ ord c - ord '0'
+        else
+          if c == '('
+            then do
+              e <- parseExprS
+              s2 <- get
+              put $ tail s2
+              return e
+            else error s
+    _ -> error s
+
+-- toPostfix :: String -> String
+-- toPostfix s =
+--   let ("", expr) = parseExpr s
+--    in reverse $ toPostfix' [] expr
+
 toPostfix :: String -> String
 toPostfix s =
-  let ("", expr) = parseExpr s
+  let expr = evalState parseExprS s
    in reverse $ toPostfix' [] expr
 
 toPostfix' :: [Char] -> Expr -> [Char]
-toPostfix' stack (Add l r) = '+' : (toPostfix' $ toPostfix'  stack l)  r
+toPostfix' stack (Add l r) = '+' : (toPostfix' $ toPostfix' stack l) r
 toPostfix' stack (Sub l r) = '-' : (toPostfix' $ toPostfix' stack l) r
 toPostfix' stack (Mul l r) = '*' : (toPostfix' $ toPostfix' stack l) r
 toPostfix' stack (Div l r) = '/' : (toPostfix' $ toPostfix' stack l) r
