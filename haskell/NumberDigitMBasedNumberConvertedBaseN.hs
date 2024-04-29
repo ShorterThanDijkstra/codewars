17e8c41d14e024a631d6554965d72f26bd9d4773
maerd-zinbieL <db1345569965@gmail.com>
2024-04-29 20:46:26 +0800

    NumberDigitMBasedNumberConvertedBaseN.hs

--- haskell/NumberDigitMBasedNumberConvertedBaseN.hs
+++ haskell/NumberDigitMBasedNumberConvertedBaseN.hs
@@ -1,16 +1,22 @@
+-- https://www.codewars.com/kata/57c18a16c82ce75f4b000020/train/haskell
 import Data.Char (ord)
-countDigit :: String -> Char -> Int -> Int -> Int
-countDigit s c i b = let i1 = parseInt s 
-                         i2 = toString i1 b 
-                     in length $ filter (c==) i2
+import Data.List (elemIndex)
+import Data.Maybe (fromJust)
+import Debug.Trace (trace)
 
-parseInt :: [Char] -> Int
-parseInt  = foldl (\acc a -> acc * 10 + ord a - ord '0') 0 
+countDigit :: String -> Char -> Int -> Int -> Int
+countDigit s digit toBase fromBase = let num = parseInt s fromBase
+                                         num2 = toString num toBase 
+                                     in length $ filter (digit==) num2
+
+parseInt :: [Char] -> Int -> Int
+parseInt s base = foldl (\acc a -> acc * base + fromJust (elemIndex a charMap)) 0 s
 
 charMap :: [Char]
 charMap = ['0'..'9'] ++ ['a' .. 'z']
 
 toString :: Int -> Int -> [Char]
+toString 0 _ = "0"
 toString i b = build "" i 
    where build acc 0 = acc 
          build acc i = let idx = i `mod` b 
