1360b729c5c3fac3ba42e98e1299c9918166709e
maerd-zinbieL <db1345569965@gmail.com>
2024-08-18 14:03:32 +0800

    wip

---
+++ haskell/JsonParser.hs
@@ -0,0 +1,7 @@
+--https://www.codewars.com/kata/55aa170b54c32468c30000a9/train/haskell
+module JSON.Parser (parse) where
+
+import JSON.Parser.Preloaded (Value(..))
+
+parse :: String -> Maybe Value
+parse = undefined
--- haskell/LowestBaseSystem.hs
+++ haskell/LowestBaseSystem.hs
@@ -5,7 +5,7 @@ allOnes 0 _ = True
 allOnes x base = x `mod` base == 1 && allOnes (x `div` base)  base
 
 getMinBase :: Int -> Int
-getMinBase n = go 3 $ ceiling $ logBase (fromIntegral 2) $ fromIntegral n
+getMinBase n = go 3 $ ceiling $ logBase 2 $ fromIntegral n
    where go i end | i == end + 1 = n - 1
                   | otherwise = let base = floor (fromIntegral n ** (1 / (fromIntegral i - 1)))
                                 in if allOnes n base 
