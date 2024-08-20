acc9532dc7e5753c8031fef1c078cd6dc08c8014
ShorterThanDijkstra <db1345569965@gmail.com>
2024-08-20 11:26:55 +0800

    wip

--- java/RailFenceCipher.java
+++ java/RailFenceCipher.java
@@ -1,18 +1,25 @@
 // https://www.codewars.com/kata/58c5577d61aefcf3ff000081/train/java
 public class RailFenceCipher { 
-     static String decode(String s, int n) {
-        int i = 0;
-        int width = n * (n - 1) / 2;
-        int gap = width + 3;
+   static String decode(String s, int n) {
+        int width = 2 * n - 2;
+        int gap = width;
+        int sIdx = 0;
         char[] res = new char[s.length()];
-        for (int j = 0; j < n; j++) {
-            gap -= 2;
-            for (int k = j; k < res.length; k += gap) {
-                res[k] = s.charAt(i);
-                i += 1;
+        for (int i = 0; i < n; i++) {
+            int j = i;
+            while (j < res.length) {
+                res[j] = s.charAt(sIdx);
+                j += gap;
+                gap = width - gap;
+                if (gap == 0) {
+                    gap = width;
+                }
+                sIdx += 1;
             }
-            if (gap == 2) {
-                gap = width + 3;
+
+            gap -= 2;
+            if (gap == 0) {
+                gap = width;
             }
         }
         return new String(res);
@@ -20,33 +27,23 @@ public class RailFenceCipher {
 
     static String encode(String s, int n) {
         StringBuilder builder = new StringBuilder();
-        int width = 2 * n;
+        int width = 2 * n - 2;
         int gap = width;
         for (int i = 0; i < n; i++) {
-            gap -= 2;
-            for (int j = i; j < s.length(); j += gap) {
+            int j = i;
+            while (j < s.length()) {
                 builder.append(s.charAt(j));
+                j += gap;
+                gap = width - gap;
+                if (gap == 0) {
+                    gap = width;
+                }
             }
-            if (gap == 2) {
+            gap -= 2;
+            if (gap == 0) {
                 gap = width;
             }
         }
         return builder.toString();
-    }
-    /*
-     * H       _     !
-     *  e     , w   d
-     *   l  o    o l
-     *    l       r
-     *
-     */
-    public static void main(String[] args) {
-        String src = "Hello, World!";
-        String tgt = "H !e,Wdloollr";
-        String encoded = encode(src, 4);
-        assert tgt.equals(encoded);
-
-        String decoded = decode(tgt, 4);
-        assert src.equals(decoded);
     }
 }
