9973ff3a2e5ff5b1f57c206a9d773c9a6d69cefd
ShorterThanDijkstra <db1345569965@gmail.com>
2024-09-14 17:59:45 +0800

    update

--- java/BinomialExpansion.java
+++ java/BinomialExpansion.java
@@ -1,5 +1,5 @@
 // https://www.codewars.com/kata/540d0fdd3b6532e5c3000b5b/train/java
-public class BinomialExpansion { 
+public class BinomialExpansion {
     private static int parseVar(int start, String s) {
         int end = start;
         while (end < s.length() && Character.isLetter(s.charAt(end))) {
@@ -34,7 +34,10 @@ public class BinomialExpansion {
         return start + 1;
     }
 
-   public static String expand(String expr) {
+    record Formula(int a, String x, int b, int m) {
+    }
+
+    private static Formula parse(String expr) {
         // (ax+b)^m
         int start = parseSpace(0, expr);
         start = parseChar('(', start, expr);
@@ -54,14 +57,82 @@ public class BinomialExpansion {
             x = expr.substring(start, end);
         }
 
-        start = parseSpace(start, expr);
-        boolean isPlus = true;
-        if (expr.charAt(start) == '-') {
-            isPlus = false;
+        start = parseSpace(end, expr);
+        int b = 0;
+        if (expr.charAt(start) == '+') {
+            start = parseSpace(start + 1, expr);
+            end = parseNumber(start, expr);
+            String s = expr.substring(start, end);
+            b = Integer.parseInt(s);
+        } else if (expr.charAt(start) == '-') {
+            start = parseSpace(start + 1, expr);
+            end = parseNumber(start, expr);
+            String s = expr.substring(start, end);
+            b = -Integer.parseInt(s);
         }
 
+        start = parseSpace(end, expr);
+        start = parseChar(')', start, expr);
+        start = parseSpace(start, expr);
+        start = parseChar('^', start, expr);
+        start = parseSpace(start, expr);
+        end = parseNumber(start, expr);
+        String s = expr.substring(start, end);
+        int m = Integer.parseInt(s);
+        return new Formula(a, x, b, m);
+    }
 
+    private static int coefficient(int m, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int num = 1;
+        int nTem = n;
+        for (int i = m; i > (m - n); i--) {
+            if (i % nTem == 0) {
+                num = num * (i / nTem);
+                nTem -= 1;
+            } else {
+                num = num * i;
+            }
+        }
+        for (int i = 2; i <= nTem; i++) {
+            num = num / i;
+        }
+        return num;
+    }
+
+    private static int powMod(int x, int y, int z) {
+        int result = 1 % z;
+        x %= z;
+        while (y != 0) {
+            if ((y & 1) == 1) {
+                result = (result * x) % z;
+            }
+            x = (x * x) % z;
+            y >>= 1;
+        }
+        return result;
+    }
+
+    private static String expand(Formula formula) {
+        for (int i = 0; i <= formula.m; i++) {
+
+        }
         return "";
     }
 
+    public static String expand(String expr) {
+        Formula formula = parse(expr);
+        return expand(formula);
+    }
+
+    public static void   public static void main(String[] args) {
+    
+  }(String[] args) {
+        String expand = expand("( x  -3 )^6");
+        int coefficient = coefficient(6, 3);
+    }
+
 }
+
