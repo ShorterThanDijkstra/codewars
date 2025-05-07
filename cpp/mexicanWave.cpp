c9cd554ff271ca68ef6f6080c57fb3dd2796e099
maerd-zinbieL <db1345569965@gmail.com>
2025-05-07 19:40:12 +0800

    update

--- cpp/mexicanWave.cpp
+++ cpp/mexicanWave.cpp
@@ -2,9 +2,33 @@
 #include <iostream>
 #include <cctype>
 #include <string>
+#include <vector>
 using namespace std;
-vector<string> wave(string y){
-   vector<string> res(y.size());
-   
-   
+vector<string> wave(string y)
+{
+   size_t size = y.size();
+   vector<string> res;
+   for (size_t i = 0; i < size; i++)
+   {
+      char c = y.at(i);
+      if (c == ' ') {
+         continue;
+      }
+      string s = y.substr(0, i);
+      s.push_back(toupper(c));
+      s += y.substr(i + 1);
+      res.push_back(s);
+   }
+   return res;
+}
+int main()
+{
+   string s = " gap ";
+   vector<string> v = wave(s);
+   cout << v.size() << endl;
+   for (string s : v)
+   {
+      cout << s << endl;
+   }
+   return 0;
 }
