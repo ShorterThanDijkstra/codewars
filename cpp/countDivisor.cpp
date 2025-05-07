4bacdc305215c7dad0c4d27baa803fbd27357674
maerd-zinbieL <db1345569965@gmail.com>
2025-05-07 20:42:20 +0800

    update

---
+++ cpp/countDivisor.cpp
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/542c0f198e077084c0000c2e/train/cpp
+#include <iostream>
+int divisors(long long n)
+{
+    int res = 1;
+    long long idx = 2;
+    long long count = 1;
+    while (idx <= n)
+    {
+        if (n % idx == 0)
+        {
+            count += 1;
+            n = n / idx;
+        } else {
+            res = res * count;
+            idx += 1;
+            count = 1;
+        }
+    }
+    res = res * count;
+    return res;
+}
+
+int main()
+{
+    std::cout << divisors(5) << std::endl;
+}
