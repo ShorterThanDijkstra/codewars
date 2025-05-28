8219c4d2c651ac170a4991a8552d95838fb23886
ShorterThanDijkstra <db1345569965@gmail.com>
2025-05-28 16:58:07 +0800

    wip

---
+++ ts/ReverseRotate.ts
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/56b5afb4ed1f6d5fb0000991/train/typescript
+export function revRot(s: string, sz: number): string {
+    const chunks: string[] = []
+    return ''
+}
